1e5abeec5f838a13b6acdf456a2b1a5a91ecc70c
seweryn <severyndub@gmail.com>
2020-05-02 17:49:42 +0200

    adding average time plot

--- measureResponsesGC/measure_response_gc.sh
+++ measureResponsesGC/measure_response_gc.sh
@@ -16,8 +16,7 @@ main(){
 
 echo $url
 echo $buildNo
-fileName="eadesign_sync_${buildNo}"
-echo $fileName
+
 URL="$url --insecure -s -o /dev/null -s -w %{time_connect}:%{time_starttransfer}:%{time_total}"
 tries=0;
 total_connect=()
@@ -28,7 +27,7 @@ counter=()
                 result=`curl $URL`
                 var=$(echo $result | awk -F":" '{print $1, $2, $3}')
                 set -- $var
-                total_connect+=("\"$1\",")
+                total_connect+=("$1,")
                 total_start+=("$2,")
                 total_time+=("$3,")
                 counter+=("\"$tries\",")
@@ -55,25 +54,30 @@ totalTimeOutputFormated=${totalTimeOutput::-1}
 echo $totalTimeOutputFormated
 
 curl --location --request POST "$url" --header 'Content-Type: application/json' \
---data-raw "{\"filename\":\"total_connect_${fileName}.png\", \"plottype\":\"line\", \"x\":[${totalConnectOutputFormated}], \"y\":[${counterOutputFormated}], \"ylab\":[\"first line\", \"second line\"]}"
+--data-raw "{\"filename\":\"total_connect_${buildNo}.png\", \"plottype\":\"line\", \"x\":[${totalConnectOutputFormated}], \"y\":[${counterOutputFormated}], \"ylab\":[\"first line\", \"second line\"]}"
 
 curl --location --request POST "$url" --header 'Content-Type: application/json' \
---data-raw "{\"filename\":\"total_start_transfer_${fileName}.png\", \"plottype\":\"line\", \"x\":[${totalStartOutputFormated}], \"y\":[${counterOutputFormated}], \"ylab\":[\"first line\", \"second line\"]}"
+--data-raw "{\"filename\":\"total_start_transfer_${buildNo}.png\", \"plottype\":\"line\", \"x\":[${totalStartOutputFormated}], \"y\":[${counterOutputFormated}], \"ylab\":[\"first line\", \"second line\"]}"
 
 curl --location --request POST "$url" --header 'Content-Type: application/json' \
---data-raw "{\"filename\":\"total_time_${fileName}.png\", \"plottype\":\"line\", \"x\":[${totalTimeOutputFormated}], \"y\":[${counterOutputFormated}], \"ylab\":[\"first line\", \"second line\"]}"
+--data-raw "{\"filename\":\"total_time_${buildNo}.png\", \"plottype\":\"line\", \"x\":[${totalTimeOutputFormated}], \"y\":[${counterOutputFormated}], \"ylab\":[\"first line\", \"second line\"]}"
 
 # Average
 
-#echo "average time connect: `echo "scale=10; $total_connect/100" | bc`";
-#echo "average time start: `echo "scale=10; $total_start/100" | bc`";
-#echo "average time taken: `echo "scale=10; $total_time/100" | bc`";
+avgTimeConn=$(echo "average time connect: `echo "scale=10; $total_connect/100" | bc`";)
+avgStartTime=$(echo "average time start: `echo "scale=10; $total_start/100" | bc`";)
+avgTakenTime=$(echo "average time taken: `echo "scale=10; $total_time/100" | bc`";)
+
+curl --location --request POST "$url" --header 'Content-Type: application/json' \
+--data-raw "{\"filename\":\"average_time_${buildNo}.png\", \"plottype\":\"line\", \"x\":["averagetimeconn","averagestarttime","averagetakentime"], \"y\":["${avgTimeConn}","${avgStartTime}","${avgTakenTime}"], \"ylab\":[\"first line\", \"second line\"]}"
+
 
 
 echo "\n"
-echo "https://storage.cloud.google.com/eadesignca1/total_connect_${fileName}.png"
-echo "https://storage.cloud.google.com/eadesignca1/total_start_transfer_${fileName}.png"
-echo "https://storage.cloud.google.com/eadesignca1/total_time_${fileName}.png"
+echo "https://storage.cloud.google.com/eadesignca1/total_connect_${buildNo}.png"
+echo "https://storage.cloud.google.com/eadesignca1/total_start_transfer_${buildNo}.png"
+echo "https://storage.cloud.google.com/eadesignca1/total_time_${buildNo}.png"
+echo "https://storage.cloud.google.com/eadesignca1/average_time_${buildNo}.png"
 
 }
 
