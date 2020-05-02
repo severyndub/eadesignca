14cd6788f343443a7f53ce2a0ecb3092c4928298
seweryn <severyndub@gmail.com>
2020-05-02 07:02:08 +0200

    testplot

--- measure_response.sh
+++ measure_response.sh
@@ -5,23 +5,23 @@
 main(){
 
 URL="http://104.155.116.131:31916/allthenews?style=plain --insecure -s -o /dev/null -s -w %{time_connect}:%{time_starttransfer}:%{time_total}"
-tries=100;
+count=100;
 total_connect=()
 total_start=()
 total_time=()
 echo " Time_Connect Time_startTransfer Time_total ";
 
-        while [ $tries -gt 0 ]; do
+        while [ $count -gt 0 ]; do
 
                 result=`curl $URL`
 #               echo $result;
                 var=$(echo $result | awk -F":" '{print $1, $2, $3}')
                 set -- $var
 
-                total_connect+=("$1,")
+                total_connect+=("$1,$count")
                 total_start+=("$2,")
                 total_time+=("$3,")
-                ((tries--))
+                ((count--))
         done
 echo "${total_connect[@]}" >> /var/lib/jenkins/workspace/eadesignca/total_connect.csv
 echo "${total_start[@]}" >> /var/lib/jenkins/workspace/eadesignca/total_start.csv
